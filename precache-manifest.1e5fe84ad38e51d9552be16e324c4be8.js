f718ac3d9e09120002e0b90cf51675aad5345d0d
Tipfurion <ovezov3210@gmail.com>
2020-06-17 17:54:00 +0300

    Updates

---
+++ precache-manifest.1e5fe84ad38e51d9552be16e324c4be8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157b3026553aa378d33d900c289a194f",
+    "url": "/mini-games/index.html"
+  },
+  {
+    "revision": "0a81e7c13d629dbae5ce",
+    "url": "/mini-games/static/css/main.dab8f93e.chunk.css"
+  },
+  {
+    "revision": "b327b228f8bb01b8839b",
+    "url": "/mini-games/static/js/2.674446eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mini-games/static/js/2.674446eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a81e7c13d629dbae5ce",
+    "url": "/mini-games/static/js/main.deb960b3.chunk.js"
+  },
+  {
+    "revision": "bd41957b9f3b4f9a7faa",
+    "url": "/mini-games/static/js/runtime-main.4bdacdf3.js"
+  },
+  {
+    "revision": "bcad8300e632307e66b5e59c6e194a8a",
+    "url": "/mini-games/static/media/arrow_left.bcad8300.svg"
+  },
+  {
+    "revision": "1670e9cbd50e5cce060890918ebe6148",
+    "url": "/mini-games/static/media/arrow_right.1670e9cb.svg"
+  },
+  {
+    "revision": "0560864b12952e51a1c97cba6f922487",
+    "url": "/mini-games/static/media/chess_250px.0560864b.png"
+  },
+  {
+    "revision": "8a9ed4b2038fac264ba2f9a6269ca821",
+    "url": "/mini-games/static/media/chess_400px.8a9ed4b2.png"
+  },
+  {
+    "revision": "6610b6c134d3aa91f0c7db769c741255",
+    "url": "/mini-games/static/media/tik_250px.6610b6c1.png"
+  },
+  {
+    "revision": "a0b40c76e2d9ba7b19c7c57c0f6cbcc5",
+    "url": "/mini-games/static/media/tik_400px.a0b40c76.png"
+  }
+]);
