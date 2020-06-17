549dad8e194b8340971f83e3df6a9490169c04ce
Tipfurion <ovezov3210@gmail.com>
2020-06-17 17:44:14 +0300

    builded

---
+++ build/precache-manifest.a93b121b5cc21d060deb6eea5e3ca4da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d59c96412509905f0d77f11474fb80d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a81e7c13d629dbae5ce",
+    "url": "/static/css/main.dab8f93e.chunk.css"
+  },
+  {
+    "revision": "b327b228f8bb01b8839b",
+    "url": "/static/js/2.674446eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.674446eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a81e7c13d629dbae5ce",
+    "url": "/static/js/main.deb960b3.chunk.js"
+  },
+  {
+    "revision": "d95dd1d6b1308e9ded2b",
+    "url": "/static/js/runtime-main.371d32f9.js"
+  },
+  {
+    "revision": "bcad8300e632307e66b5e59c6e194a8a",
+    "url": "/static/media/arrow_left.bcad8300.svg"
+  },
+  {
+    "revision": "1670e9cbd50e5cce060890918ebe6148",
+    "url": "/static/media/arrow_right.1670e9cb.svg"
+  },
+  {
+    "revision": "0560864b12952e51a1c97cba6f922487",
+    "url": "/static/media/chess_250px.0560864b.png"
+  },
+  {
+    "revision": "8a9ed4b2038fac264ba2f9a6269ca821",
+    "url": "/static/media/chess_400px.8a9ed4b2.png"
+  },
+  {
+    "revision": "6610b6c134d3aa91f0c7db769c741255",
+    "url": "/static/media/tik_250px.6610b6c1.png"
+  },
+  {
+    "revision": "a0b40c76e2d9ba7b19c7c57c0f6cbcc5",
+    "url": "/static/media/tik_400px.a0b40c76.png"
+  }
+]);
